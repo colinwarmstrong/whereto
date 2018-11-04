ff85faf3643dd078170507847fe7f3e420540480
Colin Armstrong <colinwarmstrong@gmail.com>
2018-11-04 11:33:55 -0700

    Add branch logic to the cities index endpoint to allow for filtering by state

--- app/controllers/api/v1/cities_controller.rb
+++ app/controllers/api/v1/cities_controller.rb
@@ -1,19 +1,27 @@
 class Api::V1::CitiesController < ApplicationController
   def index
-    render json: City.order(rank: :asc)
+    if city_params[:state]
+      render json: City.where(state: city_params[:state]).order(rank: :asc)
+    else
+      render json: City.order(rank: :asc)
+    end
   end
 
   def show
     if city
       render json: city
     else
-      render json: {message: "Could not find a city with id #{params[:id]}"}, status: 404
+      render json: {message: "Could not find a city with id #{city_params[:id]}"}, status: 404
     end
   end
 
   private
 
+  def city_params
+    params.permit(:id, :state)
+  end
+
   def city
-    @city ||= City.find_by_id(params[:id])
+    @city ||= City.find_by_id(city_params[:id])
   end
 end
