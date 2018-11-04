517ea9dfea958c4364c6ecd97d7cc08eaee1c1ea
Colin Armstrong <colinwarmstrong@gmail.com>
2018-11-03 20:40:16 -0600

    Configure CORS to allow the frontend to access the API

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,20 @@ module Whereto
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+        headers: :any,
+        methods: :any
+      end
+    end
+
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => 'http://my-web-service-consumer-site.com',
+      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
