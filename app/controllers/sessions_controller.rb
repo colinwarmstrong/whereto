e143d2822685195e08c177bc692d05d70bb488f4
Colin Armstrong <colinwarmstrong@gmail.com>
2018-11-05 19:37:02 -0700

    Add ability for users to login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,9 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_by_username(params[:username])
-    if user && user.authenticate(params[:password])
+    user = User.find_by_email(session_params[:email])
+    if user && user.authenticate(session_params[:password])
       session[:user_id] = user.id
-      render json: user
+      render json: user, status: 200
     else
       render json: {message: 'Invalid login credentials.'}
     end
@@ -12,5 +12,11 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     render json: {message: 'Succesfully logged out'}
+  end
+
+  private
+
+  def session_params
+    params.require(:user).permit(:email, :password)
   end
 end
--- spec/requests/authentication_endpoints_spec.rb
+++ spec/requests/authentication_endpoints_spec.rb
@@ -8,7 +8,8 @@ describe 'Authentication Endpoints' do
                         first_name: 'Colin',
                         last_name: 'Armstrong',
                         password: 'password1234'
-                      }}
+                       }
+                      }
 
       expect(User.count).to eq(0)
 
@@ -18,12 +19,56 @@ describe 'Authentication Endpoints' do
 
       new_user = JSON.parse(response.body, symbolize_names: true)
 
-      binding.pry
-      
       expect(new_user[:id]).to eq(User.last.id)
       expect(new_user[:email]).to eq(User.last.email)
       expect(new_user[:first_name]).to eq(User.last.first_name)
       expect(new_user[:last_name]).to eq(User.last.last_name)
     end
   end
+
+  context 'POST /login' do
+    it 'creates a new user when given valid credentials' do
+      user = User.create(email: 'test@test.com', first_name: 'Colin', last_name: 'Armstrong', password: 'password1234')
+
+      login_payload = {user: {
+                         email: user.email,
+                         password: user.password
+                        }
+                       }
+
+      post '/login', params: login_payload
+
+      expect(response.status).to eq(200)
+
+      returned_user = JSON.parse(response.body, symbolize_names: true)
+
+      expect(returned_user[:id]).to eq(user.id)
+      expect(returned_user[:email]).to eq(user.email)
+      expect(returned_user[:first_name]).to eq(user.first_name)
+      expect(returned_user[:last_name]).to eq(user.last_name)
+    end
+  end
+
+  # context 'POST /login' do
+  #   it 'creates a new user when given valid credentials' do
+  #     user = User.create(email: 'test@test.com', first_name: 'Colin', last_name: 'Armstrong', password: 'password1234')
+  #
+  #     login_payload = {user: {
+  #                        email: user.email,
+  #                        password: user.password
+  #                       }
+  #                      }
+  #
+  #     post '/login', params: login_payload
+  #
+  #     expect(response.status).to eq(200)
+  #
+  #     returned_user = JSON.parse(response.body, symbolize_names: true)
+  #
+  #     expect(returned_user[:id]).to eq(user.id)
+  #     expect(returned_user[:email]).to eq(user.email)
+  #     expect(returned_user[:first_name]).to eq(user.first_name)
+  #     expect(returned_user[:last_name]).to eq(user.last_name)
+  #   end
+  # end
 end
