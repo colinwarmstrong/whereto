0dc68849aa7bec3685b5a231d540b6d1bb9137ac
Colin Armstrong <colinwarmstrong@gmail.com>
2018-11-06 21:54:41 -0700

    Comment out logic in serializer because of speed issues

--- app/serializers/city_serializer.rb
+++ app/serializers/city_serializer.rb
@@ -1,29 +1,29 @@
 class CitySerializer < ActiveModel::Serializer
-  attributes :id, :name, :state, :rank, :growth, :population, :latitude, :longitude, :favorite, :rejection
+  attributes :id, :name, :state, :rank, :growth, :population, :latitude, :longitude #:favorite, :rejection
 
   def growth
     number_to_percentage(object.growth, 1)
   end
-
-  def favorite
-    if current_user && Favorite.where(user_id: current_user.id).find_by_city_id(object.id)
-      true
-    else
-      false
-    end
-  end
-
-  def rejection
-    if current_user && Rejection.where(user_id: current_user.id).find_by_city_id(object.id)
-      true
-    else
-      false
-    end
-  end
-
-  def current_user
-    @current_user ||= User.find_by_email('colinwarmstrong@gmail.com')
-  end
+  
+  # def favorite
+  #   if current_user && Favorite.where(user_id: current_user.id).find_by_city_id(object.id)
+  #     'favorite'
+  #   else
+  #     'false'
+  #   end
+  # end
+  #
+  # def rejection
+  #   if current_user && Rejection.where(user_id: current_user.id).find_by_city_id(object.id)
+  #     'rejection'
+  #   else
+  #     'false'
+  #   end
+  # end
+  #
+  # def current_user
+  #   @current_user ||= User.find_by_email('colinwarmstrong@gmail.com')
+  # end
 
   private
 
